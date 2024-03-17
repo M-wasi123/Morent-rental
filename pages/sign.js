5179d851f4e50ac0d05ecfbb859ecb86aa84dd07
M-wasi123 <wasikhatri11@gmail.com>
2024-03-17 12:06:32 +0500

    new

--- pages/sign.js
+++ pages/sign.js
@@ -32,9 +32,24 @@ switch (strength){
 }
 document.getElementById('passwordStrength').innerHTML = 'Password Strength: ' +  strengthText;
 });
-var password2 = document.getElementById('check').addEventListener('click', function (){
-    if(password2.type === "password"){
-password2.type = "text"
-password2.textcontent = "hide"
-    }else{}
-})
+// var password2 = document.getElementById('inputPassword').addEventListener('click', function (){
+//     if(password2.type === "password"){
+// password2.type = "text"
+// chek.textcontent = "hide"
+//     }else{
+//         password2.type = "password"
+//         chek.textcontent = "show"
+//     }
+// })
+function TogglePasswordShow(){
+    var password2 = document.getElementById('inputPassword');
+    var passwordShow = document.getElementById('check');
+
+    if(password2.type === 'password'){
+password2.type = "text";
+passwordShow.type = 'hide'
+    }else{
+        password2.type = 'password'
+        passwordShow.type = 'show'
+    }
+}
